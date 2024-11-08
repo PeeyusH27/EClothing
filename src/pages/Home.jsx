f6a11a6d2b8ddb48ae27f33e046a75d84be8ebd4
Peeyush <peeyushdas27@gmail.com>
2024-11-08 16:49:50 +0530

    changes added

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,14 +1,15 @@
 import Landing from '../components/Landing'
 import LatestCollection from '../components/LatestCollection'
-import BestSeller from '../components/Bestseller'
 import OurPolicy from '../components/OurPolicy'
 import NewsLetter from '../components/NewsLetter'
+import BestSeller from '../components/Bestseller'
 
 const Home = () => {
   return (
     <div>
       <Landing /> {/*HERO*/}
       <LatestCollection />
+      {/* <BestSeller /> */}
       <BestSeller />
       <OurPolicy />
       <NewsLetter />
