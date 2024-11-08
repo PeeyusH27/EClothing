489c3f926a50b289453b39db15009173669b5913
Peeyush <peeyushdas27@gmail.com>
2024-11-08 17:19:17 +0530

    changes logo and title

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -2,14 +2,13 @@ import Landing from '../components/Landing'
 import LatestCollection from '../components/LatestCollection'
 import OurPolicy from '../components/OurPolicy'
 import NewsLetter from '../components/NewsLetter'
-import BestSeller from '../components/Bestseller'
+import BestSeller from '../components/BestSeller'
 
 const Home = () => {
   return (
     <div>
       <Landing /> {/*HERO*/}
       <LatestCollection />
-      {/* <BestSeller /> */}
       <BestSeller />
       <OurPolicy />
       <NewsLetter />
